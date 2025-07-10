9ee7f9b11f7e483068d33761acda4e902d98490a
bit <git@alostbit.com>
2025-07-10 12:54:38 +0100

    linkr: updated default.nix to reflect its purpose as dev only build

--- linkr/default.nix
+++ linkr/default.nix
@@ -1,19 +1,13 @@
 { pkgs ? import <nixpkgs> {} }:
 
-let
-  repoSrc = pkgs.fetchFromGitHub {
-    repo = "box";
-    owner = "lstbit";
-    rev = "ebc601dd289180b5a9e42c68caf4981a2d548d23";
-    sha256 = "sha256-Twf/7al6ap5DSOvqomS/gK4ZNYEcsr9MMcn0UR8c9BU=";
-  };
-in
+# This bulid script is used for local development purposes only
+# for the version that gets installed check box/nix-overlay/pkgs/linkr
+
 pkgs.python313Packages.buildPythonApplication {
   pname = "linkr";
   version = "0.0";
   pyproject = true;
 
-  # src = repoSrc + "/linkr";
   src = ./.;
 
   build-system = with pkgs.python313Packages; [
